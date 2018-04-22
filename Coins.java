a3052abd484371e7edd758a49ed11df88cd7649b
cjyovich <36999000+cjyovich@users.noreply.github.com>
2018-04-22 11:57:05 -0600

    DP Min Coin Change classes
    Works mostly okay? Except in cases without a denomination of 1 (see the test case I commented out)

---
+++ Coins.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+/*
+ * Courtney Yovich
+ * April 13, 2018
+ * Class to implement greedy algorithm
+ * Minimum Coin Change Problem
+ */
+
+public class Coins {
+
+	int coins[];
+	int amount;
+
+	public int[] minCoins(int arr[], int amt) { //dynamic programming solution
+		amount = amt;
+		int numCoins[] = new int[amt + 1];
+		int coinsUsed[] = new int[amt + 1]; //this is what we'll use to store the coin amounts we need to return
+		numCoins[0] = 0; // if amt is 0, it takes zero coins
+
+		if (arr.length == 0) { //case: no coins? throw exception
+			throw new IllegalArgumentException("Array of size zero is not allowed");
+		}
+		for (int i = 1; i <= amt; i++) { //initialize array with some stuff!
+			numCoins[i] = Integer.MAX_VALUE;
+		}
+
+		for (int i = 1; i <= amt; i++) { //iterates through amount by 1
+			for (int j = 0; j < arr.length; j++) { //iterates through coin denominations
+				if (arr[j] <= i && 1 + numCoins[i - arr[j]] < numCoins[i]) {
+					numCoins[i] = 1 + numCoins[i - arr[j]];
+					coinsUsed[i] = arr[j]; //stores the coin amount
+				}
+			}
+		}
+		ArrayList<Integer> change = new ArrayList<Integer>();
+		int count = amount;
+		while(count >= 1) { //now we need to go back through and find the actual coins we used...
+			change.add(coinsUsed[count]);
+			int temp = count;
+			count = amount - coinsUsed[count];
+			amount -= coinsUsed[temp];
+		}
+		coins = new int[change.size()];
+		for (int i = 0; i < change.size(); i++) {
+			coins[i] = change.get(i);
+		}
+		return coins; //and return them!
+	}
+
+}
+
---
+++ TestChange.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/*
+ * Courtney Yovich
+ * April 13, 2018
+ * JUnit tests for class Coins 
+ * Testing the Minimum Coin Change Problem
+ */
+
+public class TestChange {
+
+	@Test
+	public void testMinCoins1() { 
+		Coins coins = new Coins();
+		int US[] = {1, 5, 10, 21, 25};
+		assertArrayEquals(new int[] {21, 21, 21}, coins.minCoins(US, 63));
+	}
+	
+	@Test
+	public void testMinCoins2() {
+		Coins coins = new Coins();
+		int lipa[] = {1, 2, 10, 20, 50};
+		assertArrayEquals(new int[] {1, 2, 2, 2, 2, 50}, coins.minCoins(lipa, 59));
+	}
+	
+	@Test
+	public void testMinCoins3() {
+		Coins coins = new Coins();
+		int US[] = {1, 5, 10, 21, 25};
+		assertArrayEquals(new int[] {10, 21, 21}, coins.minCoins(US, 52));
+	}
+	
+	@Test(expected = IllegalArgumentException.class) //array with zero coins
+	public void testEmptyArray() {
+		Coins coins = new Coins();
+		int empty[] = {};
+		coins.minCoins(empty, 0);
+	}
+	
+//	@Test
+//	public void testMinCoins() { //this one fails, because it can't get to zero?? unsure
+//		Coins coins = new Coins();
+//		int arr[] = {3, 7, 12, 15};
+//		assertArrayEquals(new int[] {7, 12}, coins.minCoins(arr, 19));
+//	}
+}
